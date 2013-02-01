6891a99467a3d9f49deff1b27f1227b2e4f33d38
Marcus Phillips <github.com@marcusphillips.com>
2013-02-01 01:32:34 -0800

    no dot access

--- validate.js
+++ validate.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
+$(function(){
 
   var formHandler = function(event){
-    console.log("submitted"); // fixme: validate form here
+    console['log']("submitted"); // fixme: validate form here
 
-    event.preventDefault(); // prevents the page from reloading
+    event['preventDefault'](); // prevents the page from reloading
   };
 
-  $("form").submit(formHandler); // register a callback
+  $("form")['submit'](formHandler); // register a callback
+
 });
