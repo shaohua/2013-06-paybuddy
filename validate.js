fd58f8b97c0cdd0bdcc5613111cc1859eaf3019c
Hack Reactor Students <github@hackreactor.com>
2013-06-29 18:52:36 -0700

    Replace 'id' with 'name'; 'name' is required

--- validate.js
+++ validate.js
@@ -3,6 +3,10 @@ $(function(){
   var formHandler = function(event){
     console['log']("submitted"); // fixme: validate form here
 
+    var data = $('form :input');
+    var da = data.serializeArray();
+    console.log(data, da);
+
     event['preventDefault'](); // prevents the page from reloading
   };
 
