f67897158e38df7e8d26f89f3e50ad0573c6e7e3
Hack Reactor Students <github@hackreactor.com>
2013-06-29 19:55:43 -0700

    Check cc number

--- validate.js
+++ validate.js
@@ -2,9 +2,27 @@ $(function(){
 
   //validation functions
   var check_cc = function(cc){
+    var valid_or_not = false;
     //expect cc as a {}
     //return true or false
-    return false;
+
+    // console.log(cc);
+
+
+    //match credit card number
+    //16 digits
+    var number_re = /\d{16}/;
+    var number_flag = !!cc.number.match(number_re);
+    if(number_flag){
+      valid_or_not = true;
+    }
+
+    //
+
+    console.log(
+      'number_flag: ', number_flag
+    );
+    return valid_or_not;
   };
 
   var toggle_error = function(msg){
@@ -21,6 +39,7 @@ $(function(){
     for(var i = 0; i < data.length; i++){
       if(data[i].name === 'number'){
         cc.number = data[i].value;
+        cc.number = cc.number.split(' ').join(''); //trim all white space
       }
       if(data[i].name === 'ccv'){
         cc.ccv = data[i].value;
@@ -37,9 +56,10 @@ $(function(){
     }
 
     console.log(cc);
+    check_cc(cc);
 
     if(!check_cc(cc)){
-      toggle_error('Credit card error.');
+      //toggle_error('Credit card error.');
     }
 
     event['preventDefault'](); // prevents the page from reloading
